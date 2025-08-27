a39abb4a049cb3dfe5d2f47ff7aea65f3e330b82
Jeremy Kruid <jdkruid@gmail.com>
2025-08-27 12:59:14 -0500

    running bell circuit

--- examples/bell_circuit.py
+++ examples/bell_circuit.py
@@ -2,13 +2,13 @@ from quantum.circuit.circuit import Circuit
 from quantum.simulator.simulator import Simulator
 
 circ = Circuit(2,2)
-# circ.apply_cx([(0,1)])
-circ.apply_h_gate([0,1])
+# circ.apply_ry([(0, (200))])
+# circ.apply_rx([(1, (300))])
+# circ.apply_rz([(0, (300))])
+
 circ.apply_x_gate([0])
-circ.apply_cx([(0,1)])
-circ.apply_x_gate([1])
-# circ.apply_cx([(0,1)])
-# circ.apply_x_gate([(1)])
+circ.apply_cx([(0, 1)])
+circ.apply_x_gate([0])
 
 copy=circ.copy()
 copy.measure_all()
